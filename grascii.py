4ff0c4b9471127a5f53a446f8e080ad9d4103a2d
Chanic Panic <chanicpanic@gmail.com>
2019-12-27 09:53:50 -0800

    first commit

---
+++ grascii.py
@@ -0,0 +1,11 @@
+
+import unittest
+
+class TestGrasci(unittest.TestCase):
+
+    def test_mat(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test_grascii.py
@@ -0,0 +1,10 @@
+
+import unittest
+
+class TestGrasci(unittest.TestCase):
+
+    def test_mat(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+if __name__ == '__main__':
+    unittest.main()

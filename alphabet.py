0449194a573f5dc80a0c4e03037cd369010c4dd3
Chanic Panic <chanicpanic@gmail.com>
2019-12-27 12:01:38 -0800

    got basic multicharacter tokenization to work

--- alphabet.py
+++ alphabet.py
@@ -79,6 +79,8 @@ tokens = {'A',
             'TM',
             'U',
             'V',
+            '/G',
+            '/K',
             '~',
             '^',
             '(',
@@ -86,10 +88,57 @@ tokens = {'A',
             '-',
             '_',
             '|',
-            '/',
             '\\',
             '\'',
             '\"',
             ',',
             '.',
             '?'}
+
+def determine_pending_symbols(tokens = tokens):
+    pending_symbols = set()
+    for item in tokens:
+        length = len(item)
+        if length > 1:
+            for i in range(length - 1):
+                pending_symbols.add(item[i])
+    return pending_symbols
+
+
+ 
+def make_tokens(grascii, tokens = tokens, max_length = 3):
+
+    def pop_stack():
+        nonlocal stack
+        nonlocal result
+        nonlocal pending_symbols
+
+        temp_token = ''.join(stack)
+        if temp_token in tokens:
+            result.append(temp_token)
+            stack = []
+        elif stack[-1] not in pending_symbols:
+            char = stack.pop()
+            pop_stack()
+            result.append(char)
+
+
+    result = []
+    pending_symbols = determine_pending_symbols(tokens)
+    stack = []
+    for char in grascii:
+        if char not in alphabet:
+            raise Exception()
+        if char in pending_symbols:
+            stack.append(char)
+        elif stack:
+            stack.append(char)
+            pop_stack()
+        elif char in tokens:
+            result.append(char)
+    pop_stack()
+    return result
+
+
+
+
--- test_grascii.py
+++ test_grascii.py
@@ -1,10 +1,39 @@
 
 import unittest
+import alphabet
 
-class TestGrasci(unittest.TestCase):
+class TestGrascii(unittest.TestCase):
 
     def test_mat(self):
         self.assertEqual('foo'.upper(), 'FOO')
+
+class TestTokenizer(unittest.TestCase):
+
+    def test_mat(self):
+        self.assertEqual(alphabet.make_tokens('MAT'),
+                ['M', 'A', 'T'])
+
+    def test_nd(self):
+        self.assertEqual(alphabet.make_tokens('ND'),
+                ['ND'])
+
+    def test_invalid_character(self):
+        self.assertRaises(Exception, alphabet.make_tokens, 'MaT')
+
+    def test_pending_symbols(self):
+        self.assertEqual(alphabet.determine_pending_symbols(['NT',
+                'MD',
+                'JNT',
+                'E',
+                'M',
+                'PND',
+                '\\']), {'N', 'J', 'P', 'M'})
+
+    def test_pending_symbols_tokens(self):
+        self.assertEqual(alphabet.determine_pending_symbols(
+                alphabet.tokens),
+                {'C', 'D', 'J', 'M', 'N', 'P', 'S', 'T', '/'}
+            )
 
 if __name__ == '__main__':
     unittest.main()

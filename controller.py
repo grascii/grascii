a13a2b05ef97eb2cf9d6eeea19c92cca9bc2c927
Chanic Panic <chanicpanic@gmail.com>
2019-12-27 18:15:05 -0800

    added arg parsing to controller

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
 
+import argparse
 import alphabet as tokenizer, transformer, compiler
 
 def grascii2regex(grascii, match_level):
@@ -6,4 +7,11 @@ def grascii2regex(grascii, match_level):
     tokens = transformer.transform(tokens, match_level)
     return compiler.generate_regex(tokens, match_level)
 
+parser = argparse.ArgumentParser(description='Search a Grascii dictionary')
+parser.add_argument('grascii', help='a grascii string to search for')
+parser.add_argument('-m', '--match-level', help='how close the search should be',
+        default=2, type=int, choices=range(1,5))
 
+args = parser.parse_args()
+
+print(grascii2regex(args.grascii, args.match_level))

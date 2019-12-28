61928fd610e1c23517c7847b21e26e4a5b766b15
Chanic Panic <chanicpanic@gmail.com>
2019-12-27 19:42:34 -0800

    added shell script to run python script and grep

---
+++ grascii.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+## begin arg parsing
+
+# saner programming env: these switches turn some bugs into errors
+set -o errexit -o pipefail -o noclobber -o nounset
+
+# -allow a command to fail with !’s side effect on errexit
+# -use return value from ${PIPESTATUS[0]}, because ! hosed $?
+! getopt --test > /dev/null 
+if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
+    echo 'I’m sorry, `getopt --test` failed in this environment.'
+    exit 1
+fi
+
+OPTIONS=d:f:m:
+LONGOPTS=dict:,file_dict:,match_level:
+
+# -regarding ! and PIPESTATUS see above
+# -temporarily store output to be able to check for errors
+# -activate quoting/enhanced mode (e.g. by writing out “--options”)
+# -pass arguments only via   -- "$@"   to separate them correctly
+! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    # e.g. return value is 1
+    #  then getopt has complained about wrong arguments to stdout
+    exit 2
+fi
+# read getopt’s output this way to handle the quoting right:
+eval set -- "$PARSED"
+
+d=grascii_dict1916.txt m=2 f=-
+# now enjoy the options in order and nicely split until we see --
+while true; do
+    case "$1" in
+        -d|--dict)
+            d=$2
+            shift 2
+            ;;
+        -f|--dict_file)
+            f=$2
+            shift 2
+            ;;
+        -m|--match_level)
+            m="$2"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Programming error"
+            exit 3
+            ;;
+    esac
+done
+
+# handle non-option arguments
+if [[ $# -ne 1 ]]; then
+    echo "$0: A single input file is required."
+    exit 4
+fi
+
+## end arg parsing
+
+regex="$(python3 controller.py -m1 $1)"
+echo $regex
+
+grep $regex $d
+

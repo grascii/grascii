887641c80c2409b64872a9e1b71d7b06008d4dd5
Chanic Panic <chanicpanic@gmail.com>
2020-06-19 14:36:22 -0700

    starting to reduce parse tree to list

--- lark_test.py
+++ lark_test.py
@@ -1,14 +1,45 @@
 
-from lark import Lark
+from lark import Lark, Visitor, Transformer, Discard
 from lark.visitors import CollapseAmbiguities
+
+class ShowTraversal(Visitor):
+    def __default__(self, tree):
+        print(tree)
+        return tree
+
+class MyTrans(Transformer):
+    def start(self, tree):
+        result = list()
+        for child in tree:
+            for token in child:
+                result.append(token)
+        return result
+
+    def __default__(self, data, children, meta):
+        result = list()
+        for child in children:
+            # for token in child:
+            result.append(child)
+        return result
+
 
 p = Lark.open("grascii.lark",
               parser="earley",
               ambiguity="explicit")
 
-test = "NTN"
+test = "A|,NT"
 tree = p.parse(test)
+# print(tree.pretty())
 for x in CollapseAmbiguities().transform(tree):
-    print(x.pretty())
+    # print(x)
+    tokens = MyTrans().transform(x)
+    print(tokens)
+    # print("".join(tokens))
+
+
+# ShowTraversal().visit(tree)
+
+
+
 
 # print(p.parse("NTN").pretty())

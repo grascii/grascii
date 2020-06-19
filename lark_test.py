80cc6d8d84242ed2cac1e3ffa2e113e59f4b4905
Chanic Panic <chanicpanic@gmail.com>
2020-06-19 15:44:27 -0700

    using python regex library on dict file'

--- lark_test.py
+++ lark_test.py
@@ -1,4 +1,5 @@
 from functools import reduce
+import re
 
 from lark import Lark, Visitor, Transformer, Discard, Token
 from lark.visitors import CollapseAmbiguities
@@ -24,6 +25,9 @@ class MyTrans(Transformer):
     
     def diphthong(self, children):
         return children[0], set(children[1:])
+    
+    # def distinguishable_consonant(self, children):
+        # return children[0], set(children[1:])
 
     def __default__(self, data, children, meta):
         result = list()
@@ -40,8 +44,8 @@ p = Lark.open("grascii.lark",
               parser="earley",
               ambiguity="explicit")
 
-test = "PNT"
-# test = "ABAK"
+# test = "PNTS)`"
+test = "ABAK"
 tree = p.parse(test)
 # print(tree.pretty())
 
@@ -61,9 +65,36 @@ trans = MyTrans()
 interpretations = [trans.transform(x) for x in trees]
 
 print("Interpretations: ", len(interpretations))
+print("Choose an interpretation")
 print("0: all")
 for i, interp in enumerate(interpretations):
-    print(i + 1, ":", interpretationToString(interp))
+    print(str(i + 1) + ":", interpretationToString(interp))
+
+def makeRegex(interp):
+    def reducer(string, token):
+        if isinstance(token, set):
+            for char in token:
+                string += char
+        else:
+            string += token 
+        return string
+
+    return reduce(reducer, interp, "")
+
+first = makeRegex(interpretations[0])
+
+# first = "A*"
+print(first)
+
+pattern = re.compile(first);
+dictionary = open("./grascii_dict1916.txt", "r")
+for line in dictionary:
+    if pattern.search(line):
+        input(line)
+    
+
+
+
     
 
 # for x in CollapseAmbiguities().transform(tree):

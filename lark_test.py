8621d905e63cf093bdfe3a2a51fe0ed1a3b6a86f
Chanic Panic <chanicpanic@gmail.com>
2020-06-19 13:14:42 -0700

    added ambiguity collapsing

--- lark_test.py
+++ lark_test.py
@@ -1,8 +1,14 @@
 
 from lark import Lark
+from lark.visitors import CollapseAmbiguities
 
-p = Lark.open("grascii.lark", 
-        parser="earley", 
-        ambiguity="explicit")
+p = Lark.open("grascii.lark",
+              parser="earley",
+              ambiguity="explicit")
 
-print(p.parse("NTN").pretty())
+test = "NTN"
+tree = p.parse(test)
+for x in CollapseAmbiguities().transform(tree):
+    print(x.pretty())
+
+# print(p.parse("NTN").pretty())

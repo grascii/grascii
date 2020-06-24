027a9a50f1136d8a723bf0079931f5176b6876ff
Chanic Panic <chanicpanic@gmail.com>
2020-06-24 13:33:05 -0700

    extracted output file function

--- build_dict.py
+++ build_dict.py
@@ -20,33 +20,35 @@ sort alphabetically?
 
 """
 
-def main(args):
-    # path = "./dict_test.txt"
-    # path = "./dsrc/grascii_dict1916.txt"
-    path = "./dsrc/z.txt"
-    dest = "./dict/"
+out_files = {}
+entry_counts = {}
+# path = "./dict_test.txt"
+# path = "./dsrc/grascii_dict1916.txt"
+path = "./dsrc/z.txt"
+dest = "./dict/"
+
+def get_output_file(grascii):
+    index = 0
+    while index < len(grascii) and grascii[index] not in string.ascii_uppercase:
+        index += 1
+    if index == len(grascii):
+        raise Exception()
+
+    char = grascii[index]
+    try:
+        result = out_files[char]
+        entry_counts[char] += 1
+        return result
+    except KeyError:
+        out_files[char] = open(dest + char, "w")
+        entry_counts[char] = 1
+        return out_files[char]
+
+def main(arguments):
 
     warnings = 0
     errors = 0
 
-    out_files = {}
-    entry_counts = {}
-    def getOutputfile(grascii):
-        index = 0
-        while index < len(grascii) and grascii[index] not in string.ascii_uppercase:
-            index += 1
-        if index == len(grascii):
-            raise Exception()
-
-        char = grascii[index]
-        try:
-            result = out_files[char]
-            entry_counts[char] += 1
-            return result
-        except KeyError:
-            out_files[char] = open(dest + char, "w")
-            entry_counts[char] = 1
-            return out_files[char]
 
     try:
         with open(path, "r") as src:
@@ -69,7 +71,7 @@ def main(args):
                     word = pair[1]
                     grascii = grascii.upper()
                     word = word.capitalize()
-                    out = getOutputfile(grascii)
+                    out = get_output_file(grascii)
                     out.write(grascii + " ")
                     out.write(word + "\n")
     finally:

652ad843fc8c669900bcc813b5276de959a45d24
Chanic Panic <chanicpanic@gmail.com>
2020-07-19 12:50:35 -0700

    reformatted similarities.py

--- similarities.py
+++ similarities.py
@@ -1,79 +1,76 @@
 
-import re
-
 equiv_nodes = {
-        "S" : ("S", "Z"),
-        "Z" : ("S", "Z"),
-        "TD" : ("TD", "DT", "DD"),
-        "DT" : ("TD", "DT", "DD"),
-        "DD" : ("TD", "DT", "DD"),
-        "MN" : ("MN", "MM"),
-        "MM" : ("MN", "MM"),
-        "TN" : ("TN", "DN"),
-        "DN" : ("TN", "DN"),
-        "TM" : ("TM", "DM"),
-        "DM" : ("TM", "DM"),
-        "NT" : ("NT", "ND"),
-        "ND" : ("NT", "ND"),
-        "MT" : ("MT", "MD"),
-        "MD" : ("MT", "MD"),
-        "DF" : ("DF", "DV", "TV"),
-        "DV" : ("DF", "DV", "TV"),
-        "TV" : ("DF", "DV", "TV"),
-        "PNT" : ("PNT", "PND", "JNT", "JND"),
-        "PND" : ("PNT", "PND", "JNT", "JND"),
-        "JNT" : ("PNT", "PND", "JNT", "JND"),
-        "JND" : ("PNT", "PND", "JNT", "JND"),
-        }
+    "S" : ("S", "Z"),
+    "Z" : ("S", "Z"),
+    "TD" : ("TD", "DT", "DD"),
+    "DT" : ("TD", "DT", "DD"),
+    "DD" : ("TD", "DT", "DD"),
+    "MN" : ("MN", "MM"),
+    "MM" : ("MN", "MM"),
+    "TN" : ("TN", "DN"),
+    "DN" : ("TN", "DN"),
+    "TM" : ("TM", "DM"),
+    "DM" : ("TM", "DM"),
+    "NT" : ("NT", "ND"),
+    "ND" : ("NT", "ND"),
+    "MT" : ("MT", "MD"),
+    "MD" : ("MT", "MD"),
+    "DF" : ("DF", "DV", "TV"),
+    "DV" : ("DF", "DV", "TV"),
+    "TV" : ("DF", "DV", "TV"),
+    "PNT" : ("PNT", "PND", "JNT", "JND"),
+    "PND" : ("PNT", "PND", "JNT", "JND"),
+    "JNT" : ("PNT", "PND", "JNT", "JND"),
+    "JND" : ("PNT", "PND", "JNT", "JND"),
+    }
 
 def get_node(stroke):
     return equiv_nodes.get(stroke, (stroke,))
 
 disconnected_nodes = [
-        ("O",),
-        ("U",),
-        get_node("PNT"),
-        get_node("DF")
-        ]
-
+    ("O",),
+    ("U",),
+    get_node("PNT"),
+    get_node("DF")
+    ]
 
 edges = [
-        ("K", "G"),
-        ("R", "L"),
-        ("L", "LD"),
-        ("T", "D"),
-        ("D", "DT"),
-        ("I", "A"),
-        ("A", "E"),
-        ("NK", "N"),
-        ("N", "M"),
-        ("M", "MN"),
-        ("N", "NG"),
-        ("M", "NK"),
-        ("NG", "NK"),
-        ("M", "NG"), # add to graph
-        ("NK", "MN"),
-        ("SH", "S"),
-        ("S", "SS"),
-        ("S", "XS"),
-        ("SS", "XS"),
-        ("XS", "X"),
-        ("X", "S"),
-        ("S", "F"),
-        ("F", "V"),
-        ("SH", "CH"),
-        ("F", "CH"),
-        ("V", "J"),
-        ("CH", "J"),
-        ("CH", "P"),
-        ("S", "P"),
-        ("P", "B"),
-        ("J", "B"),
-        ("TH", "TN"),
-        ("TN", "TM"),
-        ("NT", "MT"),
-        ("TH", "NT"),
-        ]
+    ("K", "G"),
+    ("R", "L"),
+    ("L", "LD"),
+    ("T", "D"),
+    ("D", "DT"),
+    ("I", "A"),
+    ("A", "E"),
+    ("NK", "N"),
+    ("N", "M"),
+    ("M", "MN"),
+    ("N", "NG"),
+    ("M", "NK"),
+    ("NG", "NK"),
+    ("M", "NG"), # add to graph
+    ("NK", "MN"),
+    ("SH", "S"),
+    ("S", "SS"),
+    ("S", "XS"),
+    ("SS", "XS"),
+    ("XS", "X"),
+    ("X", "S"),
+    ("S", "F"),
+    ("F", "V"),
+    ("SH", "CH"),
+    ("F", "CH"),
+    ("V", "J"),
+    ("CH", "J"),
+    ("CH", "P"),
+    ("S", "P"),
+    ("P", "B"),
+    ("J", "B"),
+    ("TH", "TN"),
+    ("TN", "TM"),
+    ("NT", "MT"),
+    ("TH", "NT"),
+    ]
 
 trans_edges = [(get_node(edge[0]), get_node(edge[1])) for edge in edges]
 
@@ -101,7 +98,7 @@ class SimilarityGraph():
         for edge in edges:
             self.add_edge(edge)
 
-    def get_similar(self, node, similarity): 
+    def get_similar(self, node, similarity):
         similars = set()
         similars.add(node)
         while similarity > 0:

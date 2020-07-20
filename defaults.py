078106d55bbc79636e51793c2c6943cdb3079129
Chanic Panic <chanicpanic@gmail.com>
2020-07-19 18:42:57 -0700

    added a file to store configuration defaults

---
+++ defaults.py
@@ -0,0 +1,15 @@
+
+SEARCH = {
+    "DictionaryPath" : "./dict",
+    "Uncertainty" : 0,
+    "SearchMode" : "match",
+    "AnnotationMode" : "discard",
+    "AspirateMode" : "discard",
+    "DisjoinerMode" : "strict"
+}
+
+BUILD = {
+    "BuildDirectory" : "./dict/",
+    "MainWordList" : "./words.txt",
+    "SupplementaryWordList" : "./extra_words.txt"
+}

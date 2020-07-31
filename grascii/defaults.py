78e6226fdb78ea7c044ff58da60535f57d2e80c0
Chanic Panic <chanicpanic@gmail.com>
2020-07-31 16:31:50 -0700

    updated defaults.py to read default and user config

--- grascii/defaults.py
+++ grascii/defaults.py
@@ -1,16 +1,12 @@
 
-SEARCH = {
-    "DictionaryPath" : "./dict/",
-    "Uncertainty" : 0,
-    "SearchMode" : "match",
-    "AnnotationMode" : "discard",
-    "AspirateMode" : "discard",
-    "DisjoinerMode" : "strict",
-    "Interpretation" : "best"
-}
+from configparser import ConfigParser
+from pathlib import Path
 
-BUILD = {
-    "BuildDirectory" : "./dict/",
-    "MainWordList" : "../words.txt",
-    "SupplementaryWordList" : "../extra_words.txt"
-}
+from grascii.config import CONF_DIRECTORY, CONF_FILE_NAME
+
+_CONFIG = ConfigParser()
+_CONFIG.read_file(Path(__file__).with_name("defaults.conf").open())
+_CONFIG.read(Path(CONF_DIRECTORY, CONF_FILE_NAME))
+
+SEARCH = _CONFIG["Search"]
+BUILD = _CONFIG["Build"]

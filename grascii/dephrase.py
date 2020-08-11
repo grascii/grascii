72245da9295c118eaa3e6415dbfe38726d8832af
Chanic Panic <chanicpanic@gmail.com>
2020-08-10 17:13:21 -0700

    inserted omitted words in receipt phrases

--- grascii/dephrase.py
+++ grascii/dephrase.py
@@ -15,6 +15,7 @@ def build_argparser(argparser: argparse.ArgumentParser) -> None:
     argparser.add_argument("phrase", action="store", 
             help="The phrase to decipher")
 
+
 class PhraseFlattener(Transformer):
     def start(self, children):
         result = list()
@@ -23,22 +24,33 @@ class PhraseFlattener(Transformer):
                 result.append(token)
         return result
 
+    def create_token(self, name: str):
+        return Token(name, name)
+
     def short_to(self, children):
-        token = type('',(object,),{"type": "TO"})()
-        result = list()
-        result.append(token)
-        return result
+        return [self.create_token("TO")]
 
     def omitted_to_noun(self, children):
         results = self.__default__(None, children, None)
-        results.append(type("", (object,), {"type": "(TO)"}))
-        results.append(type("", (object,), {"type": "(THE)"}))
+        results.append(self.create_token("(TO)"))
+        results.append(self.create_token("(THE)"))
         return results
 
     def omitted_to_verb(self, children):
         results = self.__default__(None, children, None)
-        results.append(type("", (object,), {"type": "(TO)"}))
+        results.append(self.create_token("(TO)"))
         return results
+
+    def receipt_phrase(self, children):
+        i = len(children) - 1
+        while i >= 0 and not isinstance(children[i], Token):
+            i -= 1
+        children.insert(i, self.create_token("(IN)"))
+        print(i)
+        if i < len(children) - 2:
+            children.insert(i + 2, self.create_token("(OF)"))
+            children.insert(i + 3, self.create_token("(YOUR)"))
+        return self.__default__(None, children, None)
 
     def __default__(self, data, children, meta):
         result = list()
@@ -52,16 +64,21 @@ class PhraseFlattener(Transformer):
 
 def dephase(phrase: str) -> Set[str]:
     g = get_grammar("phrases")
-    parser = Lark(g, parser="earley", ambiguity="explicit")
+    parser = Lark(g, parser="earley", ambiguity="resolve")
+    aparser = Lark(g, parser="earley", ambiguity="explicit")
+    # parser = Lark.open("./test.lark", parser="earley", ambiguity="explicit")
     trans = PhraseFlattener()
     parses: Set[str] = set()
     try:
         tree = parser.parse(phrase.upper())
+        atree = aparser.parse(phrase.upper())
     except UnexpectedInput:
         print("exception")
         return parses
 
+    # print(tree.pretty())
     trees = CollapseAmbiguities().transform(tree)
+    trees += CollapseAmbiguities().transform(atree)
     for t in trees:
         print(t.pretty())
         tokens = (token.type for token in trans.transform(t))

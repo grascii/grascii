31bb3ec39e334ea418bcff2f780f5e7274c93fca
Chanic Panic <chanicpanic@gmail.com>
2021-07-02 16:44:25 -0700

    Add interactive option to config

--- grascii/config.py
+++ grascii/config.py
@@ -10,6 +10,14 @@ from pathlib import Path, PurePath
 from shutil import copyfile
 import sys
 
+SUPPORTS_INTERACTIVE = False
+try:
+    import questionary
+    from questionary import Choice
+    SUPPORTS_INTERACTIVE = True
+except ImportError as e:
+    pass
+
 from grascii.appdirs import user_config_dir
 
 APP_NAME = "grascii"
@@ -21,7 +29,7 @@ description = "Manage Grascii configuration"
 def build_argparser(argparser: argparse.ArgumentParser) -> None: 
     """Configure an ArgumentParser parser to parse the config command-line
     options
-    
+
     :parse argparser: A fresh ArgumentParser to configure.
     """
 
@@ -34,10 +42,12 @@ def build_argparser(argparser: argparse.ArgumentParser) -> None:
             help="Print the path to the configuration file and exit.")
     argparser.add_argument("-f", "--force", action="store_true",
             help="Allow overwriting of an existing configuration file.")
+    argparser.add_argument("-i", "--interactive", action="store_true",
+            help="Run in interactive mode.")
 
 def config_exists() -> bool:
     """Check whether the user configuration file exists.
-    
+
     :returns: True if the configuration file exists.
     """
 
@@ -46,7 +56,7 @@ def config_exists() -> bool:
 def get_config_file_path() -> PurePath:
     """Get a path object representating the location of the configuration
     file.
-    
+
     :returns: A path object.
     """
 
@@ -71,6 +81,11 @@ def cli_config(args: argparse.Namespace) -> None:
     :param args: A namespace of parsed arguments.
     """
 
+    if args.interactive:
+        if not SUPPORTS_INTERACTIVE:
+            print("The interactive extra is not installed", file=sys.stderr)
+            return
+
     if args.where:
         if config_exists():
             print(get_config_file_path())
@@ -78,10 +93,16 @@ def cli_config(args: argparse.Namespace) -> None:
             print("Configuration file does not exist. Use --init to create one.", file=sys.stderr)
     elif args.init:
         if config_exists() and not args.force:
-            print("Configuration file already exists.", file=sys.stderr)
-            print("If you would like to overwrite it with the default", 
-                  "configuration, run again with --force.", file=sys.stderr)
-            return
+            if args.interactive:
+                print("Configuration file already exists.")
+                answer = questionary.confirm("Would you like to overwrite it?", default=False, auto_enter=False).ask()
+                if not answer:
+                    return
+            else:
+                print("Configuration file already exists.", file=sys.stderr)
+                print("If you would like to overwrite it with the default",
+                      "configuration, run again with --force or --interactive.", file=sys.stderr)
+                return
         create_config()
         print("Configuration file created at", get_config_file_path())
     elif args.delete:
@@ -89,9 +110,14 @@ def cli_config(args: argparse.Namespace) -> None:
             print("Configuration file does not exist.", file=sys.stderr)
             return
         if not args.force:
-            print("Are you sure you want to delete the configuration file?",
-                  "If so, run with --force.", file=sys.stderr)
-            return
+            if args.interactive:
+                answer = questionary.confirm("Are you sure you want to delete the configuration file?", default=False, auto_enter=False).ask()
+                if not answer:
+                    return
+            else:
+                print("Are you sure you want to delete the configuration file?",
+                      "If so, run with --force or --interactive.", file=sys.stderr)
+                return
         delete_config()
         print("Removed", get_config_file_path())
 

b987ae6abd15fe22988a2c8f5c3d2beb3cbed4c8
Chanic Panic <51764816+chanicpanic@users.noreply.github.com>
2022-06-20 12:59:43 -0700

    Cleanup metrics.py

--- grascii/metrics.py
+++ grascii/metrics.py
@@ -2,45 +2,48 @@
 Contains metrics for comparing search queries to regular expression matches.
 """
 
-import re
 import string
-from typing import List, Union, Set, Match, Tuple, cast
+from typing import List, Set, Match, NamedTuple
 
 from grascii import grammar
 from grascii.similarities import get_similar
 from grascii.types import Interpretation
 
+class AnnotatedStroke(NamedTuple):
+    stroke: str
+    annotations: Set[str]
 
-def convert_interpretation(interp: Interpretation) -> List[Tuple[str, Set[str]]]:
-    """Convert an interpretation into a form for comparison.
-    
+GrasciiSequence = List[AnnotatedStroke]
+
+def convert_interpretation(interp: Interpretation) -> GrasciiSequence:
+    """Convert an interpretation into a GrasciiSequence
+
     :param interp: The interpretation to convert.
-    :returns: A standard form.
+    :returns: A GrasciiSequence.
     """
 
-    new_interp: List[Tuple[str, Set[str]]] = list()
+    sequence: GrasciiSequence = []
     i = 0
     while i < len(interp):
         item = interp[i]
-        if i + 1 < len(interp):
-            if isinstance(interp[i + 1], list):
-                new_interp.append((item, set(interp[i + 1])))
-                i += 2
-                continue
-        new_interp.append((item, set()))
-        i += 1
-    return new_interp
+        assert isinstance(item, str), item
+        if i + 1 < len(interp) and isinstance(interp[i + 1], list):
+            sequence.append(AnnotatedStroke(item, set(interp[i + 1])))
+            i += 2
+        else:
+            sequence.append(AnnotatedStroke(item, set()))
+            i += 1
+    return sequence
 
 
-def convert_match(match: Match) -> List[Tuple[str, Set[str]]]:
-    """Convert an match into a form for comparison.
-    
-    :param match: The interpretation to convert.
-    :returns: A standard form.
+def convert_match(match: Match) -> GrasciiSequence:
+    """Convert an match into a GrasciiSequence
+
+    :param match: The match to convert.
+    :returns: A GrasciiSequence.
     """
 
-    new_seq: List[Tuple[str, Set[str]]] = list()
-    regex = re.compile("(" + "|".join(grammar.STROKES) + ")?(.*)")
+    sequence: GrasciiSequence = []
     for group in match.groups():
         if group is None:
             continue
@@ -48,40 +51,10 @@ def convert_match(match: Match) -> List[Tuple[str, Set[str]]]:
         while i < len(group) and group[i] in string.ascii_uppercase:
             i += 1
         if i > 0:
-            new_seq.append((group[:i], {c for c in group[i:]}))
+            sequence.append(AnnotatedStroke(group[:i], set(group[i:])))
         else:
-            new_seq.append((group, set()))
-    return new_seq
-
-def distance(s1: str, s2: str) -> int:
-    """An implementation of the W-.. algorithm to calculate Lev distance
-    based on the 2-vector Wikipedia pseudocode
-    """
-
-    m = len(s1)
-    n = len(s2)
-
-    v0 = [i for i in range(n + 1)]
-    v1 = [0 for i in range(n + 1)]
-
-    for i in range(m):
-        v1[0] = i + 1
-        
-        for j in range(n):
-            del_cost = v0[j + 1] + 1
-            ins_cost = v1[j] + 1
-            if s1[i] == s2[j]:
-                sub_cost = v0[j]
-            else:
-                sub_cost = v0[j] + 1
-
-            v1[j + 1] = min(del_cost, ins_cost, sub_cost)
-
-        tmp = v0
-        v0 = v1
-        v1 = tmp
-        
-    return v0[n]
+            sequence.append(AnnotatedStroke(group, set()))
+    return sequence
 
 BASE_COST = 4
 
@@ -97,108 +70,100 @@ COSTS = {
 ASP_COST = 1
 DIS_COST = BASE_COST
 
-def compute_ins_del_cost(tup: Tuple[str, Set[str]]) -> int:
-    """Compute the insertion and deletion cost of a stroke and its 
+def compute_ins_del_cost(stroke: AnnotatedStroke) -> int:
+    """Compute the insertion and deletion cost of a stroke and its
     annotations.
-    
-    :param tup: A stroke and its annotations for which to calculate its cost.
+
+    :param stroke: A stroke and its annotations for which to calculate its cost.
     :returns: A cost of insertion/deletion.
     """
 
     cost = 0
-    if tup[0] in grammar.STROKES:
+    if stroke.stroke in grammar.STROKES:
         cost += BASE_COST
-    elif tup[0] == grammar.ASPIRATE:
+    elif stroke.stroke == grammar.ASPIRATE:
         cost += ASP_COST
-    elif tup[0] == grammar.DISJOINER:
+    elif stroke.stroke == grammar.DISJOINER:
         cost += BASE_COST
 
-    if cost == 0:
-         print(tup)
-    assert cost > 0
-    cost += sum(COSTS.get(a, 0) for a in tup[1])
+    assert cost > 0, stroke
+    cost += sum(COSTS.get(annotation, 0) for annotation in stroke.annotations)
     return cost
 
-def get_distance(s1: str, s2: str) -> int:
+def get_distance(stroke1: str, stroke2: str) -> int:
     """Calculate the distance between two strokes in a similarity graph.
     Restricted to 0-3.
 
-    :param s1: A stroke
-    :param s2: A second stroke to compute the distance between
+    :param stroke1: A stroke
+    :param stroke2: A second stroke to compute the distance between
     :returns: A distance.
     """
 
-    def in_sim(dis):
-        sim = get_similar(s1, dis)
-        s = set()
-        for t in sim:
-            s |= {a for a in t}
-        return s2 in s
+    def in_similarity_graph(distance) -> bool:
+        similars = get_similar(stroke1, distance)
+        similar_strokes = set()
+        for tup in similars:
+            similar_strokes |= set(tup)
+        return stroke2 in similar_strokes
 
-    for i in range(3):
-        if in_sim(i):
-            return i
+    for distance in range(3):
+        if in_similarity_graph(distance):
+            return distance
     return 3
 
-def compute_sub_cost(t1: Tuple[str, Set[str]], t2: Tuple[str, Set[str]]) -> int:
+def compute_sub_cost(stroke1: AnnotatedStroke, stroke2: AnnotatedStroke) -> int:
     """Compute the cost of substituting an annotated stroke with another one.
-    
-    :param t1: The stroke to replace.
-    :param t2: The new stroke to add.
+
+    :param stroke1: The stroke to replace.
+    :param stroke2: The new stroke to add.
     :returns: A cost of substitution.
     """
 
     cost = 0
-    if t1[0] in grammar.STROKES and t2[0] in grammar.STROKES:
-        cost += get_distance(t1[0], t2[0]) * BASE_COST
-        diff = t1[1] ^ t2[1]
-        cost += sum(COSTS.get(a, 0) for a in diff)
+    if stroke1.stroke in grammar.STROKES and stroke2.stroke in grammar.STROKES:
+        cost += get_distance(stroke1.stroke, stroke2.stroke) * BASE_COST
+        diff = stroke1.annotations ^ stroke2.annotations
+        cost += sum(COSTS.get(annotation, 0) for annotation in diff)
         return cost
-    else:
-        if t1[0] == t2[0]:
-            return 0
-        return max(compute_ins_del_cost(t1), compute_ins_del_cost(t2))
+    if stroke1.stroke == stroke2.stroke:
+        return 0
+    return max(compute_ins_del_cost(stroke1), compute_ins_del_cost(stroke2))
 
-def match_distance(g1: List[Tuple[str, Set[str]]], g2: List[Tuple[str, Set[str]]]) -> int:
-    """Compute a weighed Lev distance between two sequences of annotated
-    grascii tokens.
-    
-    :param g1: A grascii sequence.
-    :param g2: A second grascii sequence.
-    :returns: The edit distance between g1 and g2.
+def match_distance(seq1: GrasciiSequence, seq2: GrasciiSequence) -> int:
+    """Compute a weighed Levenshtein distance between two sequences of annotated
+    strokes.
+
+    :param seq1: A GrasciiSequence
+    :param seq2: A second GrasciiSequence
+    :returns: A distance between seq1 and seq2.
     """
 
-    m = len(g1)
-    n = len(g2)
+    vec0 = [0]
+    for stroke in seq2:
+        vec0.append(vec0[-1] + compute_ins_del_cost(stroke))
+    vec1 = [0 for i in range(len(seq2) + 1)]
 
-    v0 = [0]
-    for i in range(n):
-        v0.append(v0[-1] + compute_ins_del_cost(g2[i]))
-    v1 = [0 for i in range(n + 1)]
+    for stroke1 in seq1:
+        vec1[0] = vec0[0] + compute_ins_del_cost(stroke1)
 
-    for i in range(m):
-        v1[0] = v0[0] + compute_ins_del_cost(g1[i])
-        
-        for j in range(n):
-            del_cost = v0[j + 1] + compute_ins_del_cost(g1[i])
-            ins_cost = v1[j] + compute_ins_del_cost(g2[j])
-            sub_cost = v0[j] + compute_sub_cost(g1[i], g2[j])
-            v1[j + 1] = min(del_cost, ins_cost, sub_cost)
+        for j, stroke2 in enumerate(seq2):
+            del_cost = vec0[j + 1] + compute_ins_del_cost(stroke1)
+            ins_cost = vec1[j] + compute_ins_del_cost(stroke2)
+            sub_cost = vec0[j] + compute_sub_cost(stroke1, stroke2)
+            vec1[j + 1] = min(del_cost, ins_cost, sub_cost)
 
-        tmp = v0
-        v0 = v1
-        v1 = tmp
-        
-    return v0[n]
+        vec0, vec1 = vec1, vec0
+
+    return vec0[len(seq2)]
 
 def standard(interp: Interpretation, match: Match) -> int:
     """Compute the standard metric for a grascii search.
-    
+
     :param interp: The interpretation to compare to the match
     :param match: The search match.
-    :returns: An edit distance.
+    :returns: A distance.
     """
 
-    g1 = convert_interpretation(interp)
-    g2 = convert_match(match)
-    return match_distance(g1, g2)
+    seq1 = convert_interpretation(interp)
+    seq2 = convert_match(match)
+    return match_distance(seq1, seq2)

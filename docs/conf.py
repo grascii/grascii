059023b2b95c8fef0dd9c59defbf89fa4e43c026
chanicpanic <51764816+chanicpanic@users.noreply.github.com>
2024-06-17 21:02:15 -0700

    Update license and copyright notices (#80)

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "Grascii"
-copyright = "2022"
+copyright = "2024"
 author = "chanicpanic"
 
 

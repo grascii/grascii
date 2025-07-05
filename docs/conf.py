ae1f22812b367e870c0dd9b77338a49747496d28
chanicpanic <51764816+chanicpanic@users.noreply.github.com>
2025-07-05 11:37:35 -0700

    Update copyright to 2025

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "Grascii"
-copyright = "2024"
+copyright = "2025"
 author = "chanicpanic"
 
 
